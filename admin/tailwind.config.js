22200fee9555b988bccaef67160fb1f8ea3c804d
gudia18 <sgudia879@gmail.com>
2025-01-08 23:46:25 +0530

    updateAdmin

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -10,7 +10,8 @@ export default {
         'auto':'repeat(auto-fill, minmax(200px, 1fr))'
       },
       colors:{
-        'primary':'#5F6FFF'
+        'primary': '#46A784'
+        // '#5F6FFF'
       }
     },
   },
