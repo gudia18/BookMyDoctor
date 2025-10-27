dd4b8529ec2c38e0567c244e52c1a7336b32f289
GUDIA RANI <121204667+gudia18@users.noreply.github.com>
2025-10-27 18:17:23 +0530

    Update backend URL in AppContextProvider
    Replaced backend URL with a hardcoded value.

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -5,8 +5,8 @@ export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
 
-    const currency = import.meta.env.VITE_CURRENCY
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const currency = import.meta.env.VITE_CURRENCY 
+    const backendUrl = https://bookmydoctor-backend-de9a.onrender.com
 
     const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
